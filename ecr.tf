cbd0e7274dcf111ad96f5e5b5735ba8d15f723c4
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-19 13:01:31 +0530

    Create ecr.tf

---
+++ ecr.tf
@@ -0,0 +1,11 @@
+resource "aws_ecr_repository" "app_repo" {
+  name = "${var.project_name}-repo"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "${var.project_name}-repo"
+  }
+}
