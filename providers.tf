a71d1b5075706a4bba0054a5249c43278a8f3626
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-19 12:33:21 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = "dev"
+      Owner       = "neha"
+      ManagedBy   = "terraform"
+    }
+  }
+}
