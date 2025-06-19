7d77ad7bf6b6cf748cce155b5b8fc24b81bade3f
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-19 12:35:36 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,1 +1,11 @@
 data "aws_availability_zones" "available" {}
+
+data "aws_ami" "ecs_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-ecs-hvm-*-x86_64-ebs"]
+  }
+}
